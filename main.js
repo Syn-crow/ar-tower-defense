2e59d8f987645f517d09448754c156b39ed6f1ce
Glitch (tower-defense-ar) <none>
2020-02-05 15:56:24 +0000

    📰🎤 Updated with Glitch

--- main.js
+++ main.js
@@ -1,45 +1,56 @@
-let marker = document.querySelector('#board');
+let marker = document.querySelector("#board");
 
-
-let path = [{"x1":0,"y1":0,"x2":4,"y2":0},{"x1":4,"y1":0,"x2":4,"y2":4},{"x1":4,"y1":4,"x2":6,"y2":4}];
+let path = [
+  { x1: 0, y1: 0, x2: 4, y2: 0 },
+  { x1: 4, y1: 0, x2: 4, y2: 4 },
+  { x1: 4, y1: 4, x2: 6, y2: 4 }
+];
 
 /*
 takes a path structured like 
 [{"x1":0,"y1":0,"x2":4,"y2":0},{"x1":4,"y1":0,"x2":4,"y2":4},{"x1":4,"y1":4,"x2":6,"y2":4}]
 and draw the path the ennemy will be walking on
 */
-function makePath(path){
-  path.forEach((box)=>{
-      let abox = document.createElement('a-entity');
-      abox.setAttribute('geometry', {
-        primitive: 'box',
-        depth: box.y2-box.y1,
-        width: box.x2-box.x1
-      });
-      abox.setAttribute('position', {x: ((box.x1+box.x2)/2), y: 0.5, z: (box.y1+box.y2)/2});
-      abox.setAttribute('material', 'color', '#808080');
-      marker.appendChild(abox);
-      let node= document.createElement('a-entity');
-      node.setAttribute('geometry', {
-        primitive: 'box',
-        depth: 1.2,
-        width: 1.2,
-        height: 1.2
-      });
-      node.setAttribute('position', {x: box.x1, y: 0.5, z: box.y1});
-      node.setAttribute('material', 'color', '#404040');
-      marker.appendChild(node);
-  })
-  let finalNode = document.createElement('a-entity');
-      finalNode.setAttribute('geometry', {
-        primitive: 'box',
-        depth: 1.2,
-        width: 1.2,
-        height: 1.2
-      });
-      finalNode.setAttribute('position', {x: path[path.length-1].x2, y: 0.5, z: path[path.length-1].y1});
-      finalNode.setAttribute('material', 'color', 'red');
-      marker.appendChild(finalNode); 
+function makePath(path) {
+  path.forEach(box => {
+    let abox = document.createElement("a-entity");
+    abox.setAttribute("geometry", {
+      primitive: "box",
+      depth: box.y2 - box.y1,
+      width: box.x2 - box.x1
+    });
+    abox.setAttribute("position", {
+      x: (box.x1 + box.x2) / 2,
+      y: 0.5,
+      z: (box.y1 + box.y2) / 2
+    });
+    abox.setAttribute("material", "color", "#808080");
+    marker.appendChild(abox);
+    let node = document.createElement("a-entity");
+    node.setAttribute("geometry", {
+      primitive: "box",
+      depth: 1.2,
+      width: 1.2,
+      height: 1.2
+    });
+    node.setAttribute("position", { x: box.x1, y: 0.5, z: box.y1 });
+    node.setAttribute("material", "color", "#404040");
+    marker.appendChild(node);
+  });
+  let finalNode = document.createElement("a-entity");
+  finalNode.setAttribute("geometry", {
+    primitive: "box",
+    depth: 1.2,
+    width: 1.2,
+    height: 1.2
+  });
+  finalNode.setAttribute("position", {
+    x: path[path.length - 1].x2,
+    y: 0.5,
+    z: path[path.length - 1].y1
+  });
+  finalNode.setAttribute("material", "color", "red");
+  marker.appendChild(finalNode);
 }
 class Ennemy {
   constructor(path) {
@@ -49,59 +60,64 @@ class Ennemy {
     this.x = this.path[0].x1;
     this.y = this.path[0].y1;
     this.id = Ennemy.id;
-    Ennemy.id +=1;
-    
-    let ennemy = document.createElement('a-entity');
-      ennemy.setAttribute('geometry', {
-        primitive: 'sphere',
-        radius: "0.5"
-      });
-      ennemy.setAttribute('position', {x: this.x, y: 1.5, z: this.y});
-      ennemy.setAttribute('material', 'color', 'red');
-      ennemy.id = "ennemy"+this.id;
-      marker.appendChild(ennemy);
+    Ennemy.id += 1;
+
+    let ennemy = document.createElement("a-entity");
+    ennemy.setAttribute("geometry", {
+      primitive: "sphere",
+      radius: "0.5"
+    });
+    ennemy.setAttribute("position", { x: this.x, y: 1.5, z: this.y });
+    ennemy.setAttribute("material", "color", "red");
+    ennemy.id = "ennemy" + this.id;
+    marker.appendChild(ennemy);
   }
-  remove(){
-    document.getElementById("ennemy"+this.id).remove();
+  remove() {
+    document.getElementById("ennemy" + this.id).remove();
   }
-  update(){
-    if(this.distance(this.x,this.y,this.path[this.pathIndex].x2,this.path[this.pathIndex].y2)<0.1){
-      this.pathIndex+=1;
-      if(this.pathIndex>this.path.length-1){
+  update() {
+    if (
+      this.distance(
+        this.x,
+        this.y,
+        this.path[this.pathIndex].x2,
+        this.path[this.pathIndex].y2
+      ) < 0.1
+    ) {
+      this.pathIndex += 1;
+      if (this.pathIndex > this.path.length - 1) {
         return true;
       }
     }
     const direction = this.getDirection();
     //console.log(direction)
-    this.x+=0.005*direction[0];
-    this.y+=0.005*direction[1];
-    let ennemy = document.getElementById("ennemy"+this.id);
-    ennemy.setAttribute('position', {x: this.x, y: 1.5, z: this.y});
+    this.x += 0.005 * direction[0];
+    this.y += 0.005 * direction[1];
+    let ennemy = document.getElementById("ennemy" + this.id);
+    ennemy.setAttribute("position", { x: this.x, y: 1.5, z: this.y });
     //console.log(this.x,this.y)
     return false;
   }
-  distance(x1,y1,x2,y2){
-    return Math.sqrt((x1-x2)**2+(y1-y2)**2);
+  distance(x1, y1, x2, y2) {
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
   }
-  getDirection(){
+  getDirection() {
     let directionY;
     let directionX;
-    
-    const dx = this.path[this.pathIndex].x1-this.path[this.pathIndex].x2;
-    if(dx!=0){
-      directionX = -dx/Math.abs(dx);
-    }
-    else{
+
+    const dx = this.path[this.pathIndex].x1 - this.path[this.pathIndex].x2;
+    if (dx != 0) {
+      directionX = -dx / Math.abs(dx);
+    } else {
       directionX = 0;
     }
-    const dy=this.path[this.pathIndex].y1-this.path[this.pathIndex].y2
-    if(dy!=0){
-      directionY = -dy/Math.abs(dy);
-    }
-    else{
+    const dy = this.path[this.pathIndex].y1 - this.path[this.pathIndex].y2;
+    if (dy != 0) {
+      directionY = -dy / Math.abs(dy);
+    } else {
       directionY = 0;
     }
-    return [directionX,directionY];
+    return [directionX, directionY];
   }
 }
 Ennemy.id = 0;
@@ -109,22 +125,21 @@ let listEnnemy = [];
 let hp = 100;
 listEnnemy.push(new Ennemy(path));
 
-function update(){
-  requestAnimationFrame( update );
+function update() {
+  requestAnimationFrame(update);
   let reached = [];
-  listEnnemy.forEach((ennemy, index)=>{
-    if(ennemy.update()){
+  listEnnemy.forEach((ennemy, index) => {
+    if (ennemy.update()) {
       reached.push(index);
     }
-  })
-  reached.forEach((ennemyReached)=>{
-    listEnnemy[ennemyReached].remove()
-    listEnnemy.splice(ennemyReached,1);
-    hp-=1
+  });
+  reached.forEach(ennemyReached => {
+    listEnnemy[ennemyReached].remove();
+    listEnnemy.splice(ennemyReached, 1);
+    hp -= 1;
     console.log(hp);
-  })
+  });
 }
 update();
-
 
 makePath(path);
--- tower.js
+++ tower.js
@@ -1,103 +1,102 @@
-            // store visibility data in object;
-            //  can only draw line when both are visible.
-            let markerVisible = { m_tower1: false, m_tower2: false, board: false };
-            
-            AFRAME.registerComponent('registerevents', {
-                init: function () 
-                {
-                    let marker = this.el;
-                    
-                    marker.addEventListener('markerFound', function() {
-                        markerVisible[ marker.id ] = true;
-                        console.log("found " + marker.id);
-                    });
-            
-                    marker.addEventListener('markerLost', function() {
-                        markerVisible[ marker.id ] = false;
-                        console.log("lost");
-                    });
-                }
-            });
-            
-            AFRAME.registerComponent('run', {
-                init: function()
-                {
-                    this.m_tower1 = document.querySelector("#m_tower1");
-                    this.m_tower2 = document.querySelector("#m_tower2");
-                    this.m_target = document.querySelector("#board");
-                    this.posTower1 = new THREE.Vector3();
-                    this.posTower2 = new THREE.Vector3();
-                    this.posTarget = new THREE.Vector3();
-                    this.selectedEnnemy = 0;
+// store visibility data in object;
+//  can only draw line when both are visible.
+let markerVisible = { m_tower1: false, m_tower2: false, board: false };
 
-                    let scene = document.querySelector('a-scene').object3D;
-                    
-                    // Line 1
-                    this.geometry1 = new THREE.Geometry();
-                    this.geometry1.vertices.push( new THREE.Vector3(-1,-1,-1) );
-                    this.geometry1.vertices.push( new THREE.Vector3( 1, 1, 1) );
-                    this.material1 = new THREE.LineBasicMaterial( {color: 0x00b5e1, linewidth: 5} );
-                    this.line1 = new THREE.Line( this.geometry1, this.material1 );
-                    scene.add( this.line1 );
+AFRAME.registerComponent("registerevents", {
+  init: function() {
+    let marker = this.el;
 
-                    // Line 2
-                    this.geometry2 = new THREE.Geometry();
-                    this.geometry2.vertices.push( new THREE.Vector3(-1,-1,-1) );
-                    this.geometry2.vertices.push( new THREE.Vector3( 1, 1, 1) );
-                    this.material2 = new THREE.LineBasicMaterial( {color: 0x8f00ff, linewidth: 5} );
-                    this.line2 = new THREE.Line( this.geometry2, this.material2 );
-                    scene.add( this.line2 );
-                },
+    marker.addEventListener("markerFound", function() {
+      markerVisible[marker.id] = true;
+      console.log("found " + marker.id);
+    });
 
-                
-                tick: function (time, deltaTime) 
-                {   
-                    if ( markerVisible["m_tower1"] && markerVisible["board"] )
-                    {   
-                        this.m_tower1.object3D.translateY(3.1);
-                        this.m_target.object3D.translateX(listEnnemy[0].x);
-                        this.m_target.object3D.translateY(1.4);
-                        this.m_target.object3D.translateZ(listEnnemy[0].y);
-                        this.m_tower1.object3D.getWorldPosition(this.posTower1); 
-                        this.m_target.object3D.getWorldPosition(this.posTarget);
-                        this.m_tower1.object3D.translateY(-3.1);
-                        this.m_target.object3D.translateX(-listEnnemy[0].x);
-                        this.m_target.object3D.translateY(-1.4);
-                        this.m_target.object3D.translateZ(-listEnnemy[0].y);
-                        this.geometry1.vertices[0] = this.posTower1;
-                        this.geometry1.vertices[1] = this.posTarget;
-                        this.geometry1.verticesNeedUpdate = true;
-                        if (this.posTower1.distanceTo(this.posTarget) < 4){
-                            this.line1.visible = true;
-                        }
-                        else{
-                            this.line1.visible = false;
-                        }
-                    }
-                    else
-                    {
-                        this.line1.visible = false;
-                    }
-                    if ( markerVisible["m_tower2"] && markerVisible["m_target"] )
-                    {   
-                        this.m_tower2.object3D.translateY(3.1);
-                        this.m_target.object3D.translateY(0.4);
-                        this.m_tower2.object3D.getWorldPosition(this.posTower2); 
-                        this.m_target.object3D.getWorldPosition(this.posTarget);
-                        this.m_tower2.object3D.translateY(-3.1);
-                        this.m_target.object3D.translateY(-0.4);
-                        this.geometry2.vertices[0] = this.posTower2;
-                        this.geometry2.vertices[1] = this.posTarget;
-                        this.geometry2.verticesNeedUpdate = true;
-                        if (this.posTower2.distanceTo(this.posTarget) < 4){
-                            this.line2.visible = true;
-                        }else{
-                            this.line2.visible = false;
-                        }
-                    }
-                    else
-                    {
-                        this.line2.visible = false;
-                    }
-                }
-            });
+    marker.addEventListener("markerLost", function() {
+      markerVisible[marker.id] = false;
+      console.log("lost");
+    });
+  }
+});
+
+AFRAME.registerComponent("run", {
+  init: function() {
+    this.m_tower1 = document.querySelector("#m_tower1");
+    this.m_tower2 = document.querySelector("#m_tower2");
+    this.m_target = document.querySelector("#board");
+    this.posTower1 = new THREE.Vector3();
+    this.posTower2 = new THREE.Vector3();
+    this.posTarget = new THREE.Vector3();
+    this.selectedEnnemy = 0;
+
+    let scene = document.querySelector("a-scene").object3D;
+
+    // Line 1
+    this.geometry1 = new THREE.Geometry();
+    this.geometry1.vertices.push(new THREE.Vector3(-1, -1, -1));
+    this.geometry1.vertices.push(new THREE.Vector3(1, 1, 1));
+    this.material1 = new THREE.LineBasicMaterial({
+      color: 0x00b5e1,
+      linewidth: 5
+    });
+    this.line1 = new THREE.Line(this.geometry1, this.material1);
+    scene.add(this.line1);
+
+    // Line 2
+    this.geometry2 = new THREE.Geometry();
+    this.geometry2.vertices.push(new THREE.Vector3(-1, -1, -1));
+    this.geometry2.vertices.push(new THREE.Vector3(1, 1, 1));
+    this.material2 = new THREE.LineBasicMaterial({
+      color: 0x8f00ff,
+      linewidth: 5
+    });
+    this.line2 = new THREE.Line(this.geometry2, this.material2);
+    scene.add(this.line2);
+  },
+
+  tick: function(time, deltaTime) {
+    if (
+      markerVisible["m_tower1"] &&
+      markerVisible["board"] &&
+      listEnnemy.length != 0
+    ) {
+      this.m_tower1.object3D.translateY(3.1);
+      this.m_target.object3D.translateX(listEnnemy[0].x);
+      this.m_target.object3D.translateY(1.4);
+      this.m_target.object3D.translateZ(listEnnemy[0].y);
+      this.m_tower1.object3D.getWorldPosition(this.posTower1);
+      this.m_target.object3D.getWorldPosition(this.posTarget);
+      this.m_tower1.object3D.translateY(-3.1);
+      this.m_target.object3D.translateX(-listEnnemy[0].x);
+      this.m_target.object3D.translateY(-1.4);
+      this.m_target.object3D.translateZ(-listEnnemy[0].y);
+      this.geometry1.vertices[0] = this.posTower1;
+      this.geometry1.vertices[1] = this.posTarget;
+      this.geometry1.verticesNeedUpdate = true;
+      if (this.posTower1.distanceTo(this.posTarget) < 4) {
+        this.line1.visible = true;
+      } else {
+        this.line1.visible = false;
+      }
+    } else {
+      this.line1.visible = false;
+    }
+    if (markerVisible["m_tower2"] && markerVisible["m_target"]) {
+      this.m_tower2.object3D.translateY(3.1);
+      this.m_target.object3D.translateY(0.4);
+      this.m_tower2.object3D.getWorldPosition(this.posTower2);
+      this.m_target.object3D.getWorldPosition(this.posTarget);
+      this.m_tower2.object3D.translateY(-3.1);
+      this.m_target.object3D.translateY(-0.4);
+      this.geometry2.vertices[0] = this.posTower2;
+      this.geometry2.vertices[1] = this.posTarget;
+      this.geometry2.verticesNeedUpdate = true;
+      if (this.posTower2.distanceTo(this.posTarget) < 4) {
+        this.line2.visible = true;
+      } else {
+        this.line2.visible = false;
+      }
+    } else {
+      this.line2.visible = false;
+    }
+  }
+});
